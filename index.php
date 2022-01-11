98e521ffca687a5f741137ae35b0a5d42d3298c0
jseo123 <jose123135@gmail.com>
2022-01-11 20:26:22 +0100

    Added basic style to index

--- index.php
+++ index.php
@@ -7,16 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/index.css">
 </head>
+
 <body>
 
-<main class="d-flex justify-content-center align-items-center">
-    <div class="d-flex flex-row justify-content-around">
-    <div class="client">
-        <a><p>Cliente</p></a>
-    </div>
-    <div class="admin "> <a><p>Administrador</p></a></div>
-    </div>
-</main>
+    <main class="d-flex justify-content-center align-items-center">
+        <div class="d-flex flex-row justify-content-around">
+            <div class="client border d-flex justify-content-between">
+                <a class="nav-link redirect d-flex justify-content-between">
+                    <p>Cliente</p>
+                </a>
+            </div>
+            <div class="admin border d-flex justify-content-between"> <a class="nav-link redirect d-flex justify-content-between">
+                    <p>Administrador</p>
+                </a></div>
+        </div>
+    </main>
 
 </body>
+
 </html>
