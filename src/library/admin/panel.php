313a65ef933ab4d58846f4481b697ff9476b4428
jseo123 <jose123135@gmail.com>
2022-01-12 02:03:43 +0100

    Created page for saving new restaurants

--- src/library/admin/panel.php
+++ src/library/admin/panel.php
@@ -6,7 +6,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="../../../assets/js/admin.js"></script>
+    <script src="../../../assets/js/admin.js" defer></script>
     <link rel="stylesheet" href="../../../assets/css/admin.css">
 </head>
 
@@ -17,9 +17,9 @@
     $user = $_SESSION["admin"];
     echo "<h3>Buen dia $user!</h3>";
     ?>
-<main class="d-flex justify-content-center">
-<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a class="btn btn-outline-success"><p>Crear nuevo restaurante</p></a></div>
-<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a class="btn btn-outline-success"><p>Crear empleado</p></a></div>
+<main id="container" class="d-flex justify-content-center">
+<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a href="newRestaurant.php" id="newRestaurant" class="btn btn-outline-success">Crear nuevo restaurante</a></div>
+<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a id="newEmployee" class="btn btn-outline-success">Crear empleado</a></div>
 </main>
 </body>
 </html>
