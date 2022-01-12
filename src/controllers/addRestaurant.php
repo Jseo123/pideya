0b6f87bec3595fd550b82d5527da4c7333cb1241
jseo123 <jose123135@gmail.com>
2022-01-12 21:02:34 +0100

    Made header visible

--- src/controllers/addRestaurant.php
+++ src/controllers/addRestaurant.php
@@ -5,5 +5,5 @@ if(isset($_POST)){
     $restaurant = $_POST;
     unset($_POST);
 addRestaurant($restaurant);
-//header("Location:../library/admin/panel.php?addedRestaurant");
+header("Location:../library/admin/panel.php?addedRestaurant");
 }
