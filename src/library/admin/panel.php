e6e13d931a3065326188f2c3352fc00a72ab319c
jseo123 <jose123135@gmail.com>
2022-01-12 01:20:09 +0100

    Added flex to create new restaurants and employees

--- src/library/admin/panel.php
+++ src/library/admin/panel.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -6,18 +5,23 @@
     <title>Admin Panel</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../../../assets/js/admin.js"></script>
     <link rel="stylesheet" href="../../../assets/css/admin.css">
 </head>
+
 <body>
-<?php require_once  "../html/header.html";
-require_once "../../controllers/adminLogControl.php";
-
-sessionCheck();
-
-?>
-
+    <?php require_once  "../html/header.html";
+    require_once "../../controllers/adminLogControl.php";
+    sessionCheck();
+    $user = $_SESSION["admin"];
+    echo "<h3>Buen dia $user!</h3>";
+    ?>
+<main class="d-flex justify-content-center">
+<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a class="btn btn-outline-success"><p>Crear nuevo restaurante</p></a></div>
+<div class="flex-item d-flex justify-content-center navbar navbar-light bg-light"><a class="btn btn-outline-success"><p>Crear empleado</p></a></div>
+</main>
 </body>
 </html>
-</body>
 
-</html>
+
--- src/library/admin/statistics.php
+++ src/library/admin/statistics.php
@@ -5,6 +5,8 @@
     <title>Admin Panel</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../../../assets/js/admin.js"></script>
     <link rel="stylesheet" href="../../../assets/css/admin.css">
 </head>
 <body>
@@ -14,7 +16,4 @@ sessionCheck();
 ?>
 
 </body>
-</html>
-</body>
-
 </html>
